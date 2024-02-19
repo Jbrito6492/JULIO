7bff1bc7267e5da9009423ee9eb458ba82217008
Josh Brito <jbrito6492@gmail.com>
2024-02-18 23:49:21 -0700

    (S)  - Add voice input form and create route for voice input

--- app/controllers/voice_input_controller.rb
+++ app/controllers/voice_input_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class VoiceInputController < ApplicationController
+  def create
+    redirect_to dashboard_url, notice: 'Voice input was successfully processed.'
+  end
 end
