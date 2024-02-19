b0dfa6dcb28a3a083afce9160a498216f2d729c3
Josh Brito <jbrito6492@gmail.com>
2024-02-19 00:42:48 -0700

    (B)  - Create welcome audio file in Dashboard#index view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+require 'openai'
 class DashboardController < ApplicationController
   def index
-    greetings = ['Buenos Dias Pendejo']
+    response = OpenAI::Client.new.audio.speech(
+      parameters: {
+        model: 'tts-1',
+        input: "Welcome to JULIO, viejón! The time is #{Time.zone.now.strftime('%H:%M on %B %d, %Y')}.",
+        voice: 'fable'
+      }
+    )
+    File.binwrite(Rails.root.join('public', 'welcome.mp3'), response)
+    @audio_path = '/welcome.mp3'
   end
 end
--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -2,5 +2,4 @@
 
 OpenAI.configure do |config|
   config.access_token = ENV['OPENAI_ACCESS_TOKEN']
-  config.api_version = 'gpt-3.5-turbo'
 end
