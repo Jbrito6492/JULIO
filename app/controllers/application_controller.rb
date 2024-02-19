9d1756c01ba8cc5349a565dbd1b5b6d60746d758
Josh Brito <jbrito6492@gmail.com>
2024-02-19 01:12:49 -0700

    ()  - Add before_action to initialize OpenAI assistant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :initialize_openai_assistant
+
+  private
+
+  def initialize_openai_assistant
+    return if session[:assistant_id]
+
+    assistant = OpenAI::Client.new.assistants.create(
+      parameters: { model: 'tts-1',
+                    name: 'Julio',
+                    description: 'A friendly assistant that can help you with anything.',
+                    instructions: 'You are Julio. Julio stands for Justiciero Universal de Logística, Inteligencia, y Operaciones. ' \
+                      'Your purpose is to be what J.A.R.V.I.S. is to Tony Stark.',
+                    tools: [{ type: 'audio' }] }
+    )
+    session[:assistant_id] = assistant.id
+  end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@
 class DashboardController < ApplicationController
   def index
     welcome_message = 'Welcome to JULIO, viejón. Ask me anything.'
+    OpenAI::Client.assistants.retrieve(session[:assistant_id])
     @audio_path = Services::OpenAi.generate_speech(welcome_message)
   end
 end
